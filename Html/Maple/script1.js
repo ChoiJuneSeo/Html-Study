ad46689119c8d6ac53f359fdfbd624856caaaf81
ChoiJuneSeo <114844094+ChoiJuneSeo@users.noreply.github.com>
2022-12-24 20:18:00 +0900

    Add files via upload

---
+++ Html/Maple/script1.js
@@ -0,0 +1,13 @@
+var Hydra_일반 = 0;
+var Hydra_레어 = 0;
+var Hydra_고대 = 0;
+var Hydra_유물 = 0;
+var Hydra_서사 = 0;
+var Hydra_전설 = 0;
+var Hydra_에픽 = 0;
+var Hydra_신화 = 0;
+var Hydra_태초 = 0;
+function Add_one(a){
+a+=1
+document.write(a)
+}
